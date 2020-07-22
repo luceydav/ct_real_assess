a354036b08d3379178bc71ccc8bbf9be1b388c82
DAVID LUCEY <davidlucey@iMac.home>
2020-07-22 10:38:23 -0400

    update plan

--- R/plan.R
+++ R/plan.R
@@ -4,14 +4,11 @@ the_plan <-
   drake_plan(
     raw_99_11 = try(load_raw_data()),
     cleaned_01_recent = load_socrata(),
-    new = 
-      merge_and_filter_sources(
-        raw_99_11,
-        cleaned_01_recent
-        ) %>%
-      final_cleanup() %>%
-      add_reval(),
-    saveRDS(new, file_out("ct_sales_99_2018.RDS")),
+    new = merge_and_clean_sources(
+      raw_99_11,
+      cleaned_01_recent
+    ), 
+    save_file = saveRDS(new, file_out("ct_sales_99_2018.RDS")),
     deployment = rsconnect::deployApp(
       appFiles = file_in(
         "ct_sales_99_2018.RDS",
